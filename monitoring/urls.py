16135de4524b037d24f254c754ab1f9336139ade
research94iman <research94iman@gmail.com>
2024-05-14 10:49:47 +0330

    fix(url): map / url to admin page

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -35,6 +35,7 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
+    path('', admin.site.urls),
     path('admin/', admin.site.urls),
     path('data/', include('apps.dataStoring.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
